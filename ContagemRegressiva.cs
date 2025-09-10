820453aaded36b543486050032fa0a0aaacd9315
Camila Pereira <129539701+CamilaaSilvaa@users.noreply.github.com>
2025-09-10 09:42:45 -0300

    Create ContagemRegressiva.cs

---
+++ ContagemRegressiva.cs
@@ -0,0 +1,16 @@
+using System;
+class prog1
+{
+    static void Main()
+    {
+        int i = 11;
+
+        while (i > 1)
+        {
+            i--;
+            Console.WriteLine(i);
+        }
+
+       Console.WriteLine("Feliz Ano Novo!");
+    }
+}
